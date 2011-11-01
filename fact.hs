b9dc5927819b4b443e27bf5d670a6ab4a79e2cc8
MigOrec <MigOrec@gmail.com>
2011-11-01 12:42:02 +0400

    error handeling

--- fact.hs
+++ fact.hs
@@ -1,5 +1,6 @@
 module Fact where
 
 fact :: Integer -> Integer
-fact 1 = 1
-fact n = n* fact(n-1)
+fact 0 = 1
+fact n |n>0 = n* fact(n-1)
+       | otherwise = error "Попытка вычислить фактопиал отрицательного числа"
